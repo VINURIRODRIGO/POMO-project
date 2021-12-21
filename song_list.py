c7d97a0656666175ae4a0c9f8009f6d2ef92f237
VINURIRODRIGO <77739211+VINURIRODRIGO@users.noreply.github.com>
2021-12-22 02:45:21 +0530

    Update song_list.py

--- song_list.py
+++ song_list.py
@@ -3,12 +3,7 @@ ALARM_PATH = "songs_collection/alarm"
 SONGS = [f"{PATH}/Affections Across Time.mp3",
          f"{PATH}/Beautiful Chinese Music.mp3",
          f"{PATH}/BEST MUSIC RELAX.mp3",
-         f"{PATH}/chinese castle.mp3",
          f"{PATH}/Chinese girl with umbrella.mp3",
-         f"{PATH}/Coffee cup.mp3",
-         f"{PATH}/Girl near the balcony.mp3",
-         f"{PATH}/Morning Relaxing Music.mp3",
-         f"{PATH}/Napoleon Hill.mp3",
-         f"{PATH}/videoplayback (6).mp3"
+         f"{PATH}/Girl near the balcony.mp3"
          ]
 ALARM = f'{ALARM_PATH}/song.mp3'
